5ad4410107141ace1311bd2e32f65f7c89033c25
williamsalex <williams_alex@protonmail.com>
2020-03-28 18:37:15 -0400

    fixing broken link

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
             <h3>Contact Me</h3>
             <a href = "mailto:williams_alex@pm.me">Email</a>
             <br/>
-            <a href = "github.com/williamsalex">GitHub</a>
+            <a href = "https://github.com/williamsalex">GitHub</a>
             <br/>
             <a href = "https://www.linkedin.com/in/alexander-williams-823925182/">LinkedIn</a>
         </div>
