807b84e8be45f6a3b824aa4125026dfe6010800a
williamsalex <williams_alex@protonmail.com>
2020-03-28 17:44:09 -0400

    finished adding content to all tabs

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,10 @@ export default function About() {
     return(
         <div className = 'text'>
             <p>FROM BROOKLYN, NY</p>
-            <p>I am a software developer </p>
+            <p>I enjoy working on tough problems with interesting people.</p>
+            <p>I've been a self-taught developer since 2017, taking several courses and a bootcamp in addition to studying in my free time.</p>
+            <p>Outside of software, I enjoy reading, math and long distance running.</p>
+            <p>As of the 28<sup>th</sup> of March, I am reading Lee Kuan Yew by Allison & Blackwill, and looking for an entry-level development job.</p>
         </div>
     )
 }
--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,12 @@ import '../stylesheets/homepage.scss'
 export default function Contact() {
     return(
         <div className = 'text'>
-        
+            <h3>Contact Me</h3>
+            <a href = "mailto:williams_alex@pm.me">Email</a>
+            <br/>
+            <a href = "github.com/williamsalex">GitHub</a>
+            <br/>
+            <a href = "https://www.linkedin.com/in/alexander-williams-823925182/">LinkedIn</a>
         </div>
     )
 }
